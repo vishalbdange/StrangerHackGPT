708c5889b4373ac5f95b463b57a5b1111e6636e7
Neel Dandiwala <neeldandi@gmail.com>
2023-04-01 19:22:58 +0530

    added api/threeButton

---
+++ api/threeButton.py
@@ -0,0 +1,45 @@
+import requests
+import json
+from deep_translator import GoogleTranslator
+
+url = "https://iqwhatsapp.airtel.in:443/gateway/airtel-xchange/basic/whatsapp-manager/v1/session/send/interactive/buttons"
+
+def sendThreeButton(receiver, langId, text, tag, title, sessionId):
+    if langId != 'en':
+        text = GoogleTranslator(source="en", target=langId).translate(text)
+        title[0] = GoogleTranslator(source="en", target=langId).translate(title[0]) + '(' + tag[0] +')'
+        title[1] = GoogleTranslator(source="en", target=langId).translate(title[1]) + '(' + tag[1] +')'
+        title[2] = GoogleTranslator(source="en", target=langId).translate(title[2]) + '(' + tag[2] +')'
+    
+    payload = json.dumps({
+        "sessionId": sessionId,
+        "to": receiver,
+        "from": "918904587734",
+        "message": {
+            "text": text
+        },
+        "buttons": [
+            {
+                "tag": tag[0],
+                "title": title[0]
+            },
+            {
+                "tag": tag[1],
+                "title": title[1] 
+            },
+            {
+                "tag": tag[2],
+                "title": title[2]
+            },
+        ]
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic QUlSVEVMX0RJR19MeEk4VFVLOGVLZnRCa3lQdWlmTDoxeipMVTZLTjxrenNMPytiVzM2'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+# 919820860959
